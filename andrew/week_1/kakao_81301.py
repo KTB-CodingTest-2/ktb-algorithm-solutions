fc44127c60d6ea40a902a9227a4e65e92ef0c50e
JoEun <whdms1107@gmail.com>
2024-08-08 09:15:39 +0900

    숫자 문자열

---
+++ andrew/week_1/kakao_81301.py
@@ -0,0 +1,17 @@
+def solution(s):
+    word_dict = {
+        '0': 'zero',
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine'
+    }
+    for num, word in word_dict.items():
+        s = s.replace(word, num)
+
+    return int(s)
