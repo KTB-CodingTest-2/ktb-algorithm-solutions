0f378375b869787073b5a73f8851607873b94802
cjm0611 <cjm-0611@naver.com>
2024-08-14 12:49:12 +0900

    feat: 2주차 recursion 문제 풀이

---
+++ lily/week_2/boj_6603.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+
+readline = sys.stdin.readline
+input = readline().rstrip('\n')
+while input != '0':
+    arr = list(map(int, input.split()))
+    k = arr[0]
+    arr = arr[1:]
+    LOTTO_LEN = 6
+    combs = combinations(arr, LOTTO_LEN)
+    for comb in combs:
+        output = ' '.join(map(str, comb))
+        print(output)
+    print()
+    input = readline().rstrip('\n')
