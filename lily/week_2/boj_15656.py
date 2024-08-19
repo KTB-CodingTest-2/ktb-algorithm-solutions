cf071f336dc74ca3cbe8ac1f0bf5cd981cf93605
cjm0611 <cjm-0611@naver.com>
2024-08-20 01:12:29 +0900

    fix: 사용하지 않는 변수 삭제

--- lily/week_2/boj_15656.py
+++ lily/week_2/boj_15656.py
@@ -2,14 +2,13 @@ import sys
 
 N, M = 0, 0
 sequence = []
-visited = []
 answer = []
 
 def back(cnt):
-    global N, M, sequence, visited, answer
+    global N, M, sequence, answer
 
     if cnt == M:
-        print(' '.join(map(str, answer)))
+        print(*answer)
         return
     
     for i in range(N):
@@ -21,10 +20,5 @@ readline = sys.stdin.readline
 N, M = map(int, readline().split())
 sequence = list(map(int, readline().split()))
 sequence.sort()
-visited = [False] * N
 
-if M == 1:
-    output = '\n'.join(map(str, sequence))
-    print(output)
-else:
-    back(0)
+back(0)
